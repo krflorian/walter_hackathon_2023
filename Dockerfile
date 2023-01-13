c6bdec4baba70be83ada61e68d35afdb7ffb2831
florian.krempl <florian.krempl@alexanderthamm.com>
2023-01-13 10:14:54 +0100

    agent

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,10 @@ FROM python:alpine
 
 WORKDIR /app
 
-COPY README.md setup.py .
+COPY README.md setup.py requirements.txt .
 COPY src /app/src
 
+RUN pip install -r requirements.txt
 RUN pip install .
 
 EXPOSE 8080
--- src/truck_agent/main.py
+++ src/truck_agent/main.py
@@ -4,16 +4,38 @@ from .api import *
 
 app = FastAPI()
 
+diesel_price = 2.023
+diesel_consumption = 25
+
+
+def calculate_profit(offer: CargoOffer):
+    cost = offer.km_to_cargo * diesel_price
+    return offer.price - cost
+
+
 @app.post("/decide", response_model=DecideResponse)
 def decide(req: DecideRequest) -> DecideResponse:
     """
-    See https://app.swaggerhub.com/apis-docs/walter-group/walter-group-hackathon-sustainable-logistics/1.0.0 for 
+    See https://app.swaggerhub.com/apis-docs/walter-group/walter-group-hackathon-sustainable-logistics/1.0.0 for
     a detailed description of this endpoint.
     """
+
     if req.offers:
-        return DecideResponse(command="DELIVER", argument=req.offers[0].uid)
+        command = "DELIVER"
     else:
+        command = "ROUTE"
+
+    ##########################################
+    if command == "DELIVER":
+        offer = req.offers[0]
+        return DecideResponse(command="DELIVER", argument=offer.uid)
+    elif command == "ROUTE":
+
+        return DecideResponse(command="ROUTE", argument="Berlin")
+    else:
+
         return DecideResponse(command="SLEEP", argument=1)
+
 
 def main():
     uvicorn.run(app, host="0.0.0.0", port=8080, log_level="debug")
